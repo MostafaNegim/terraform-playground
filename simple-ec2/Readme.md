28e126562efe1937c9e3372d7c00a6efd066cb67
Mostafa <mostafa.negim@gmail.com>
2021-12-10 22:11:09 +0330

    Reconfigure

---
+++ simple-ec2/Readme.md
@@ -0,0 +1,9 @@
+```bash
+
+export AWS_ACCESS_KEY_ID="anaccesskey"
+export AWS_SECRET_ACCESS_KEY="asecretkey"
+export AWS_DEFAULT_REGION="us-west-2"
+
+terraform init
+terraform plan -out
+```
--- simple-ec2/main.tf
+++ simple-ec2/main.tf
@@ -1,1 +1,31 @@
-provider{}
+provider "aws" {
+    region = "us-east-1"
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+#   filter {
+#     name   = "virtualization-type"
+#     values = ["hvm"]
+#   }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "simple-ec2" {
+  	ami 		= data.aws_ami.ubuntu.id
+  	instance_type	= "t2.micro"
+    tags = {
+        Name = "Simple-ec2"
+    }
+}
+
+resource "aws_eip" "ip" {
+    instance = aws_instance.simple-ec2.id
+}
