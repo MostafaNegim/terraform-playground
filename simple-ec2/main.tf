68f5384b178185c85494bc2caca8ab3086fe7e4d
Mostafa Negim <mostafa.negim@gmail.com>
2022-08-25 22:24:28 +0430

    feat: rearrange dir

--- simple-ec2/main.tf
+++ simple-ec2/main.tf
@@ -1,8 +1,19 @@
 provider "aws" {
-    region = "us-east-1"
+  region = var.region
 }
 
-data "aws_ami" "ubuntu" {
+resource "aws_instance" "jump_server" {
+  ami           = data.aws_ami.ubuntu_amis.id
+  instance_type = "t2.micro"
+  tags = {
+    Name = var.ec2_name
+  }
+  # security_group = [ "${aws_security_group.sg.id}" ]
+  vpc_security_group_ids = [ "${aws_security_group.sg.id}" ]
+  key_name = aws_key_pair.kp.key_name
+}
+
+data "aws_ami" "ubuntu_amis" {
   most_recent = true
 
   filter {
@@ -10,69 +21,14 @@ data "aws_ami" "ubuntu" {
     values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
   }
 
-#   filter {
-#     name   = "virtualization-type"
-#     values = ["hvm"]
-#   }
+  #   filter {
+  #     name   = "virtualization-type"
+  #     values = ["hvm"]
+  #   }
 
   owners = ["099720109477"] # Canonical
 }
 
-resource "aws_instance" "simple-ec2" {
-  	ami 		= data.aws_ami.ubuntu.id
-  	instance_type	= "t2.micro"
-    tags = {
-        Name = "Simple-ec2"
-    }
-      user_data     = <<EOT
-#cloud-config
-# update apt on boot
-package_update: true
-# install nginx
-packages:
-- nginx
-write_files:
-- content: |
-    <!DOCTYPE html>
-    <html>
-    <head>
-      <title>StackPath - Amazon Web Services Instance</title>
-      <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
-      <style>
-        html, body {
-          background: #000;
-          height: 100%;
-          width: 100%;
-          padding: 0;
-          margin: 0;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          flex-flow: column;
-        }
-        img { width: 250px; }
-        svg { padding: 0 40px; }
-        p {
-          color: #fff;
-          font-family: 'Courier New', Courier, monospace;
-          text-align: center;
-          padding: 10px 30px;
-        }
-      </style>
-    </head>
-    <body>
-      <img src="https://www.stackpath.com/content/images/logo-and-branding/stackpath-logo-standard-screen.svg">
-      <p>This request was proxied from <strong>Amazon Web Services</strong></p>
-    </body>
-    </html>
-  path: /usr/share/app/index.html
-  permissions: '0644'
-runcmd:
-- cp /usr/share/app/index.html /usr/share/nginx/html/index.html
-EOT
-
-}
-
 resource "aws_eip" "ip" {
-    instance = aws_instance.simple-ec2.id
+    instance = aws_instance.jump_server.id
 }
--- simple-ec2/variables.tf
+++ simple-ec2/variables.tf
@@ -1,3 +1,33 @@
 variable "region" {
   default = "us-east-1"
 }
+
+variable "ec2_name" {
+  type        = string
+  description = "EC2 name"
+  default = "ansible_host"
+}
+
+variable "aws_properties" {
+  type = map
+  default = {
+    public_ip = true
+    key_name = "myseckey"
+    secgroupname = "IAC-Sec-Group"
+  }
+}
+
+# variable "remote_state_address" {
+#   type        = string
+#   description = "Gitlab remote state file address"
+# }
+
+# variable "username" {
+#   type        = string
+#   description = "Gitlab username to query remote state"
+# }
+
+# variable "access_token" {
+#   type        = string
+#   description = "GitLab access token to query remote state"
+# }
