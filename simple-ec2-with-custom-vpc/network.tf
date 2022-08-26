3b2bff92912f0ff3e82d1af56549b2c176c6c771
Mostafa Negim <mostafa.negim@gmail.com>
2022-08-26 13:47:28 +0430

    feat: add eip

--- simple-ec2-with-custom-vpc/network.tf
+++ simple-ec2-with-custom-vpc/network.tf
@@ -1,7 +1,7 @@
 resource "aws_vpc" "vpc" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block           = "10.0.0.0/16"
   enable_dns_hostnames = true
-  enable_dns_support = true
+  enable_dns_support   = true
   tags = {
     "Name" = "vpc"
   }
@@ -9,26 +9,31 @@ resource "aws_vpc" "vpc" {
 
 # subnets
 resource "aws_subnet" "subnet" {
-  cidr_block = "${cidrsubnet(aws_vpc.vpc.cidr_block, 3, 1)}"
-  vpc_id = "${aws_vpc.vpc.id}"
+  cidr_block        = cidrsubnet(aws_vpc.vpc.cidr_block, 3, 1)
+  vpc_id            = aws_vpc.vpc.id
   availability_zone = "${var.region}a"
 }
 
 resource "aws_security_group" "sg" {
-  name = "sg"
-  vpc_id = "${aws_vpc.vpc.id}"
-  ingress = [ {
-    cidr_blocks = [ "0.0.0.0/0" ]
+  name   = "sg"
+  vpc_id = aws_vpc.vpc.id
+  ingress = [{
+    cidr_blocks = ["0.0.0.0/0"]
     description = "Ingress Description"
-    from_port = 22
-    protocol = "tcp"
-    to_port = 22
-  } ]
-  egress = [ {
-    cidr_blocks = [ "0.0.0.0/0" ]
+    from_port   = 22
+    protocol    = "tcp"
+    to_port     = 22
+  }]
+  egress = [{
+    cidr_blocks = ["0.0.0.0/0"]
     description = "Egress Description"
-    from_port = 0
-    protocol = "-1"
-    to_port = 0
-  } ]
+    from_port   = 0
+    protocol    = "-1"
+    to_port     = 0
+  }]
+}
+
+resource "aws_eip" "eip" {
+  instance = "${aws_instance.ec2.id}"
+  vpc = true
 }
