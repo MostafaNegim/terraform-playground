99bcbe8c704ad26bbf7e900684ced5a26c205df4
Mostafa Negim <mostafa.negim@gmail.com>
2022-08-26 21:49:04 +0430

    feat: add versions.tf

---
+++ simple-eks/versions.tf
@@ -0,0 +1,13 @@
+terraform {
+  backend "http" {
+
+  }
+
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"
+        versversion = "~> 4.15.0"
+    }
+  }
+  required_version = "~> 1.2.0"
+}
