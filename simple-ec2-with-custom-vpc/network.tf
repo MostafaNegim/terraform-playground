2dfe32151e293394bb7ea9dd9537520cb6332cd4
Mostafa Negim <mostafa.negim@gmail.com>
2022-08-26 13:49:58 +0430

    fix: remove "

--- simple-ec2-with-custom-vpc/network.tf
+++ simple-ec2-with-custom-vpc/network.tf
@@ -28,7 +28,7 @@ resource "aws_security_group" "sg" {
     cidr_blocks = ["0.0.0.0/0"]
     description = "Egress Description"
     from_port   = 0
-    protocol    = "-1"
+    protocol    = -1
     to_port     = 0
   }]
 }
