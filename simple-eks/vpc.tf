9e513f1214ebde880dcfad13e9ccdb6d70b5c72a
Mostafa Negim <mostafa.negim@gmail.com>
2022-08-26 22:19:55 +0430

    fix: typo

--- simple-eks/vpc.tf
+++ simple-eks/vpc.tf
@@ -5,7 +5,7 @@ module "vpc" {
   name = "eks-vpc"
 
   cidr = "10.0.0.0/16"
-  azs  = slice(data.available.names, 0, 3)
+  azs  = slice(data.aws_availability_zones.available.names, 0, 3)
 
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
@@ -15,12 +15,12 @@ module "vpc" {
   enable_dns_hostnames = true
 
   public_subnet_tags = {
-    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    # "kubernetes.io/cluster/${local.cluster_name}" = "shared"
     "kubernetes.io/role/elb"                      = 1
   }
 
   private_subnet_tags = {
-    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    # "kubernetes.io/cluster/${local.cluster_name}" = "shared"
     "kubernetes.io/role/internal-elb"             = 1
   }
 }
