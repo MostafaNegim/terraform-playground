d4d2c516cca1670c7c78cda26a1be9b18125e385
Mostafa Negim <mostafa.negim@gmail.com>
2022-09-02 16:04:28 +0430

    fix: change number to string

--- module-s3/main.tf
+++ module-s3/main.tf
@@ -22,7 +22,7 @@ module "ec2_instance" {
   source  = "terraform-aws-modules/ec2-instance/aws"
   version = "~> 4.0"
 
-  for_each               = toset([1, 2])
+  for_each               = toset(["one", "two"])
   name                   = "my-ec2-cluster-${each.key}"
   ami                    = "ami-ebd02392"
   instance_type          = "t2.micro"
